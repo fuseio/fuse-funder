159710e5992e0ba795cb9f00f8a93a6e2bdbf0e4
Leon Prouger <leonprou@gmail.com>
2018-12-10 10:47:50 +0200

    working with fuse network

--- app/controllers/BalanceController.js
+++ app/controllers/BalanceController.js
@@ -1,30 +1,24 @@
 module.exports = (osseus) => {
+  const balance = async ({ account }) => {
+    const { web3, token } = osseus.lib
+    const native = web3.utils.fromWei(await web3.eth.getBalance(account))
+    const cryptoFiat = web3.utils.fromWei(await token.methods.balanceOf(account).call())
+    return {
+      native,
+      cryptoFiat
+    }
+  }
   return {
     request: async (req, res) => {
-      const { web3, token } = osseus.lib
-      const { account } = req.params
-      await web3.eth.sendTransaction({
+      await osseus.lib.web3.eth.sendTransaction({
         from: osseus.config.ethereum_from_account,
-        to: account,
+        to: req.params.account,
         value: osseus.config.ethereum_native_bonus
       })
-      const balance = await web3.eth.getBalance(account)
-      const fcfBalance = await token.methods.balanceOf(account).call()
-      res.send({
-        native: web3.utils.fromWei(balance),
-        fcf: fcfBalance
-      })
+      res.send(await balance(req.params))
     },
     balance: async (req, res) => {
-      const { web3, token } = osseus.lib
-      const { account } = req.params
-
-      const native = web3.utils.fromWei(await web3.eth.getBalance(account))
-      const fcf = web3.utils.fromWei(await token.methods.balanceOf(account).call())
-      res.send({
-        native,
-        fcf
-      })
+      res.send(await balance(req.params))
     }
   }
 }
