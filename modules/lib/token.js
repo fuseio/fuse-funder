2f343134e93671eb5e6940044d139067a97f1da7
Leon Prouger <leonprou@gmail.com>
2018-12-11 12:06:17 +0200

    change configs and ABI

--- modules/lib/token.js
+++ modules/lib/token.js
@@ -2,7 +2,7 @@ const path = require('path')
 
 module.exports = (osseus) => {
   const { web3 } = osseus.lib
-  const abi = require(path.join('../../', osseus.config.web3_token_abi_path)).abi
+  const abi = require(path.join('../../', osseus.config.web3_token_abi_path))
   const token = new web3.eth.Contract(abi, osseus.config.ethereum_token_address)
   return token
 }
