0777a76a73cd74c91812a8f49fb7067acca61457
Leon Prouger <leonprou@gmail.com>
2018-12-13 13:47:04 +0200

    chage number of tries in tests

--- tests/doubleFunding.js
+++ tests/doubleFunding.js
@@ -1,7 +1,7 @@
 const request = require('request')
 const Web3 = require('web3')
 
-const NUM_OF_TRIES = 1
+const NUM_OF_TRIES = 5
 const REQUEST_URL = 'http://localhost:8080/api/balance/request'
 
 const test = () => {
