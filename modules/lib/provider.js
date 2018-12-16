b57740db448d83cd84895f89e66cf68f543afbf1
Leon Prouger <leonprou@gmail.com>
2018-12-16 12:06:29 +0200

    delete env variable usage

--- modules/lib/provider.js
+++ modules/lib/provider.js
@@ -1,6 +1,6 @@
 const HDWalletProvider = require('truffle-hdwallet-provider')
 
 module.exports = (osseus) => {
-  const mnemonic = osseus.config.ethereum_hd_wallet_mnemonic || process.env.ETHEREUM_HD_WALLET_MNEMONIC
+  const mnemonic = osseus.config.ethereum_hd_wallet_mnemonic
   return new HDWalletProvider(mnemonic, osseus.config.ethereum_web3_provider, osseus.config.ethereum_hd_wallet_index)
 }
