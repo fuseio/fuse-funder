cd471a69cc8896d57a8a1fba61e70a46f2243dea
Lior <liorag@colu.com>
2019-12-12 11:21:50 +0200

    fund fuse base on networkType

--- modules/lib/funding/native.js
+++ modules/lib/funding/native.js
@@ -2,20 +2,29 @@ const request = require('request-promise-native')
 const { get } = require('lodash')
 
 module.exports = (osseus, agenda) => {
-  const getNativeBonus = async ({ accountAddress, tokenAddress }) => {
+  const getNativeBonus = async ({ accountAddress, tokenAddress, networkType }) => {
     if (!tokenAddress) {
       return osseus.config.ethereum_native_user_bonus
     }
     const response = await request.get(`${osseus.config.fuse_studio_api_base}/tokens/${tokenAddress}`)
     const owner = get(JSON.parse(response), 'data.owner')
-    return owner === accountAddress ? osseus.config.ethereum_native_admin_bonus : osseus.config.ethereum_native_user_bonus
+    const isAdmin = owner === accountAddress
+    if (isAdmin) {
+      if (networkType === 'ropsten') {
+        return osseus.config.ethereum_native_admin_bonus_ropsten
+      } else {
+        return osseus.config.ethereum_native_admin_bonus_mainnet
+      }
+    } else {
+      return osseus.config.ethereum_native_user_bonus
+    }
   }
 
-  const fundNative = async ({ accountAddress, tokenAddress }) => {
+  const fundNative = async ({ accountAddress, tokenAddress, networkType }) => {
     const fundingAccount = await osseus.db_models.account.lockAccount()
     const provider = osseus.lib.provider.create(fundingAccount.childIndex)
     const web3 = osseus.lib.web3.create(provider)
-    const nativeBonus = await getNativeBonus({ accountAddress, tokenAddress })
+    const nativeBonus = await getNativeBonus({ accountAddress, tokenAddress, networkType })
     try {
       let tx = await web3.eth.sendTransaction({
         from: fundingAccount.address,
@@ -38,13 +47,13 @@ module.exports = (osseus, agenda) => {
     if (!job || !job.attrs || !job.attrs.data) {
       return done(new Error(`Job data undefined`))
     }
-    let { accountAddress, tokenAddress } = job.attrs.data
+    let { accountAddress, tokenAddress, networkType } = job.attrs.data
     if (!accountAddress) {
       return done(new Error(`Job data is missing "accountAddress"`))
     }
 
     try {
-      let tx = await fundNative({ accountAddress, tokenAddress })
+      let tx = await fundNative({ accountAddress, tokenAddress, networkType })
       done(null, tx)
     } catch (err) {
       done(err)
